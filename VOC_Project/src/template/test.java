ad3a52c7a5fcf39cc46a57ff8357993b4b1dcde2
DESKTOP-0M8NFOF\MyHome <jihyeKim97@github.com>
2021-05-14 01:28:50 +0900

    <success>

--- VOC_Project/src/template/test.java
+++ VOC_Project/src/template/test.java
@@ -2,10 +2,6 @@ package template;
 
 public class test {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("����test");
-=======
-		System.out.println("지원지원성공성공 기원기원!!!!!!!test");
->>>>>>> 5007e9e658f6142d40b802167e4591c4341e603c
+		System.out.println("지혜test");
 	}
 }
