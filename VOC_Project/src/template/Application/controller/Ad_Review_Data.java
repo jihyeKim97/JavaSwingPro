f267ebfd8dc032a00fe9bb0b8bc290a7761f541d
youv132 <youv132@github.com>
2021-06-03 19:07:15 +0900

    <주현> 리뷰수정 및 공지사항 등록및 수정

---
+++ VOC_Project/src/template/Application/controller/Ad_Review_Data.java
@@ -0,0 +1,38 @@
+package template.Application.controller;
+
+public class Ad_Review_Data {
+
+	private int review_id;
+	private String content;
+	public Ad_Review_Data(String content) {
+		super();
+		this.content = content;
+	}
+	public Ad_Review_Data(int review_id) {
+		super();
+		this.review_id = review_id;
+	}
+	public Ad_Review_Data(int review_id, String content) {
+		super();
+		this.review_id = review_id;
+		this.content = content;
+	}
+	public int getReview_id() {
+		return review_id;
+	}
+	public void setReview_id(int review_id) {
+		this.review_id = review_id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	@Override
+	public String toString() {
+		return "Ad_Review_Data [review_id=" + review_id + ", content=" + content + "]";
+	}
+
+}
+
