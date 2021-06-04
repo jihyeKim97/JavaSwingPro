0fdd5ade971c580d61b8ab1bfe2e444fd4291db3
youv132 <youv132@github.com>
2021-06-04 17:45:30 +0900

    <주현> 영화정보 수정

--- VOC_Project/src/template/Application/controller/DB/Ad_Film_DB.java
+++ VOC_Project/src/template/Application/controller/DB/Ad_Film_DB.java
@@ -1,69 +1,122 @@
 package template.Application.controller.DB;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
 import template.Application.controller.Data.Ad_Film_Data;
+import template.Application.controller.Data.Ad_Notice_data;
+import template.Application.controller.Data.Notice_data;
 
 public class Ad_Film_DB {
+	static ArrayList<Ad_Film_Data> FilmArr = new ArrayList<>();
 	static DB_Connect DB;
 	static Connection conn;
-	public static final int FIND_ERROR = 1;
-	public static final int FIND_SUCCESS = 2;
-	public static final int FIND_NULL = 3;
-	public static final int PHN_MISMATCH = 4;
-	
-	
-	
+	Ad_Film_Data ad_film;
+
 	public Ad_Film_DB() {
 		this.conn = DB.getConn();
 	}
-	
-	public Ad_Film_Data selectMovie(String mvposter) {
-		if (this.conn != null) {
 
-			String sql = "select * from movies where image_file_name = ?";
+	public static ArrayList<Ad_Film_Data> GetFilm(int movieid) {
+		if (conn != null) {
+			String sql = "SELECT * FROM movies where MOVIES_id = " + movieid;
+			try {
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				while (rs.next()) {
+					String title = rs.getString("title");
+					String genre = rs.getString("genre");
+					String director = rs.getString("director");
+					String age_group = rs.getString("age_group");
+					String story = rs.getString("story");
+					String average_score = rs.getString("average_score");
+					String gee = rs.getString("gee");
+					String open_date = rs.getString("open_date");
+					String production = rs.getString("production");
+					String schedule_date = rs.getString("schedule_date");
+					String image_file_name = rs.getString("IMAGE_FILE_NAME");
+					String running_time = rs.getString("running_time");
+					
+					FilmArr.add(new Ad_Film_Data(title, genre, director, age_group, story, average_score, gee, open_date, production, schedule_date,image_file_name,running_time));
+
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return FilmArr;
+	}
+	
+	
+	public boolean changeFilm(int movieid,String title, String genre, String director, String age_group,
+			String story, String average_score, String gee, String open_date, String production, String schedule_date,
+			String image_file_name, String running_time) {
+		if (this.conn != null) {
+			String sql = "UPDATE MOVIES SET TITLE = ? ,GENRE = ?, DIRECTOR = ?, AGE_GROUP = ?, STORY = ?,"
+					+ "AVERAGE_SCORE = ?, GEE = ?, OPEN_DATE = ?, PRODUCTION = ?, SCHEDULE_DATE = ?, "
+					+ "IMAGE_FILE_NAME = ?, RUNNING_TIME = ? WHERE MOVIES_ID = "+movieid;
 			try {
 				PreparedStatement pstmt = conn.prepareStatement(sql);
-				pstmt.setString(1, mvposter);
-				ResultSet rs = pstmt.executeQuery();
-				if (rs.next()) {
-					Ad_Film_Data mv = new Ad_Film_Data(rs.getString("title"), rs.getString("genre"),rs.getString("director"), rs.getInt("age_group"),
-							rs.getString("story"), rs.getInt("average_score"), rs.getString("gee"),rs.getDate("open_date"),
-							rs.getString("production"),rs.getDate("schedule_date"),rs.getInt("schedule_time"),rs.getString("running_time"));
-					return mv;	
+				pstmt.setString(1, title);
+				pstmt.setString(2, genre);
+				pstmt.setString(3, director);
+				pstmt.setString(4, age_group);
+				pstmt.setString(5, story);
+				pstmt.setString(6, average_score);
+				pstmt.setString(7, gee);
+				pstmt.setString(8, open_date);
+				pstmt.setString(9, production);
+				pstmt.setString(10, schedule_date);
+				pstmt.setString(11, image_file_name);
+				pstmt.setString(12, running_time);
+			
+				int rs = pstmt.executeUpdate();
+				if (rs == 1) {
+					System.out.println("db 영화 수정");
+					return true;
 				} else {
-					
+					System.out.println("db 영화 수정 실패");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+	public  boolean  insertNewMovie(Ad_Film_Data ui) {
+		
+		if (this.conn != null && ui != null) {
+			String sql 
+					= "INSERT INTO movies(mo_id,title,content,viewcount,member_id) VALUES (MEMBER_SEQ.nextval,"
+							+"'"+ ui.getTitle() + "', '"
+							+ ui.getGenre() + "', '" + "0" + "', '" + 24 + "')";
+			System.out.println(sql);
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				
+				int r = pstmt.executeUpdate();
+				if (r == 1) {
+					System.out.println("DBMgr: 공지사항 등록 성공! " + ui);
+					return true;
+				} else {
+					System.out.println("DBMgr: 공지사항 등록 실패! " + ui);
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		} else {
-			System.out.println("DB 통신 에러");
+			System.out.println("DB error!!");
 		}
-		return null;
-	}
-	
-	public int setmovie(String mvposter) {
-		if (mvposter == null || mvposter.isEmpty()) {
-			return FIND_NULL;
-		}
-		if (this.conn != null) {
-			Ad_Film_Data mb = selectMovie(mvposter);
-			if (mb != null) {
-					String mbtitle = mb.getTitle();
-				JOptionPane.showMessageDialog(null, "데이터를 불러옵니다.");
-						return FIND_SUCCESS;
-			} else {
-				return FIND_ERROR;
-			}
-		} else {
-			System.out.println("DB 통신 에러!!");
-		}
-		return FIND_ERROR;
-	}
+		return false;
+}
+
 }
--- VOC_Project/src/template/Application/view/admin/Admin_FilmManagement.java
+++ VOC_Project/src/template/Application/view/admin/Admin_FilmManagement.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -21,10 +22,14 @@ import javax.swing.border.EmptyBorder;
 import template.Application.controller.DB.Ad_Film_DB;
 import template.Application.controller.DB.Login_DB;
 import template.Application.controller.Data.Ad_Film_Data;
+import template.Application.controller.Data.Ad_Notice_data;
 import template.Application.controller.btn.RoundedButtonD;
 
 import java.awt.BorderLayout;
 import javax.swing.border.LineBorder;
+
+import sun.java2d.pipe.SpanShapeRenderer.Simple;
+
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
@@ -34,6 +39,10 @@ import javax.swing.JTextArea;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class Admin_FilmManagement extends JDialog{
 
@@ -49,10 +58,14 @@ public class Admin_FilmManagement extends JDialog{
 	private JTextField ad_txt_runtime;
 	JLabel ad_lb_load_poster;
 	Admin_FilmManagement dlg;
-	private JTextField ad_txt_schedule_date;
-	private JTextField ad_txt_schedule_time;
+	JLabel ad_lb_poster;
 	Ad_Film_Data afd = new Ad_Film_Data();
-	Ad_Film_DB AFD = new Ad_Film_DB();
+	Ad_Film_DB db = new Ad_Film_DB();
+	int film_id = 258;
+	File imgFile;
+	ArrayList<Ad_Film_Data> MyArr = new ArrayList<>();
+	JTextField ad_txt_schedule_date;
+	JTextField ad_txt_img;
 	/**
 	 * Launch the application.
 	 */
@@ -80,6 +93,7 @@ public class Admin_FilmManagement extends JDialog{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		MyArr = db.GetFilm(film_id);
 		frame = new JFrame();
 		frame.setTitle("영화정보");
 		frame.setBounds(100, 100, 493, 804);
@@ -103,8 +117,9 @@ public class Admin_FilmManagement extends JDialog{
 		panel_2.add(pnMenuImage);
 		pnMenuImage.setLayout(new BorderLayout(0, 0));
 		
-		JLabel ad_lb_poster = new JLabel("");
+		ad_lb_poster = new JLabel("");
 		pnMenuImage.add(ad_lb_poster, BorderLayout.CENTER);
+
 		
 //		JLabel lbMenuImgPath = new JLabel("no path");
 		ad_lb_load_poster = new JLabel("no path");
@@ -120,7 +135,7 @@ public class Admin_FilmManagement extends JDialog{
 				 = new JFileChooser(currentDirPath);					
 				if( openDlg.showOpenDialog(dlg) 
 						== JFileChooser.APPROVE_OPTION ) {
-					File imgFile = openDlg.getSelectedFile();
+					imgFile = openDlg.getSelectedFile();
 					System.out.println("선택된 파일명: " + imgFile.getName());
 					System.out.println("선택된 파일경로명: " + imgFile.getPath());
 					
@@ -135,8 +150,7 @@ public class Admin_FilmManagement extends JDialog{
 					ic.setImage(icImg);
 					ad_lb_poster.setIcon(ic);
 					ad_lb_poster.repaint();
-					int find = AFD.setmovie(ad_lb_poster.getText());
-					
+
 				}
 			}
 		});
@@ -146,46 +160,62 @@ public class Admin_FilmManagement extends JDialog{
 		JPanel panel_4 = new JPanel();
 		panel_4.setBounds(204, 10, 250, 35);
 		panel_2.add(panel_4);
-		panel_4.setLayout(new BorderLayout(0, 0));
+		panel_4.setLayout(null);
 		
-		JLabel lblNo = new JLabel("      상영날짜            상영시간");
+		JLabel lblNo = new JLabel("      상영날짜   ");
+		lblNo.setBounds(0, 0, 250, 35);
 		lblNo.setFont(new Font("굴림", Font.BOLD, 15));
-		panel_4.add(lblNo, BorderLayout.CENTER);
+		panel_4.add(lblNo);
 		
-		JPanel panel_5 = new JPanel();
-		panel_5.setBounds(202, 102, 252, 148);
-		panel_2.add(panel_5);
-		panel_5.setLayout(new BorderLayout(0, 0));
+//		SimpleDateFormat smf = new SimpleDateFormat("yyyy-MM-");
+		String a[] = MyArr.get(0).getSchedule_date().split(" ");
+		ad_txt_schedule_date = new JTextField();
+		ad_txt_schedule_date.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
+		ad_txt_schedule_date.setHorizontalAlignment(SwingConstants.CENTER);
+		ad_txt_schedule_date.setColumns(10);
+		ad_txt_schedule_date.setBounds(108, 0, 130, 35);
+		panel_4.add(ad_txt_schedule_date);
+		ad_txt_schedule_date.setText(a[0]);
 		
-		JTextArea ad_txt_story = new JTextArea();
-		ad_txt_story.setText((String) null);
-		ad_txt_story.setLineWrap(true);
-		ad_txt_story.setEditable(false);
-		panel_5.add(ad_txt_story, BorderLayout.CENTER);
+		
 		
 		JPanel panel_7 = new JPanel();
+		panel_7.setBounds(202, 87, 252, 27);
+		panel_2.add(panel_7);
 		panel_7.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panel_5.add(panel_7, BorderLayout.NORTH);
 		
 		JLabel label_1 = new JLabel("줄거리");
 		panel_7.add(label_1);
 		
-		JPanel panel_6 = new JPanel();
-		panel_6.setLayout(null);
-		panel_6.setBounds(204, 57, 250, 35);
-		panel_2.add(panel_6);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(202, 111, 252, 139);
+		panel_2.add(scrollPane);
 		
-		ad_txt_schedule_date = new JTextField();
-		ad_txt_schedule_date.setHorizontalAlignment(SwingConstants.CENTER);
-		ad_txt_schedule_date.setBounds(25, 0, 82, 35);
-		panel_6.add(ad_txt_schedule_date);
-		ad_txt_schedule_date.setColumns(10);
+		JTextArea textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		scrollPane.setViewportView(textArea);
+		textArea.setText(MyArr.get(0).getStory());
 		
-		ad_txt_schedule_time = new JTextField();
-		ad_txt_schedule_time.setHorizontalAlignment(SwingConstants.CENTER);
-		ad_txt_schedule_time.setColumns(10);
-		ad_txt_schedule_time.setBounds(156, 0, 82, 35);
-		panel_6.add(ad_txt_schedule_time);
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setBounds(204, 50, 250, 35);
+		panel_2.add(panel_1);
+		
+		JLabel label = new JLabel("       이미지");
+		label.setFont(new Font("굴림", Font.BOLD, 15));
+		label.setBounds(0, 0, 250, 35);
+		panel_1.add(label);
+		
+		ad_txt_img = new JTextField();
+		ad_txt_img.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
+		ad_txt_img.setHorizontalAlignment(SwingConstants.CENTER);
+		ad_txt_img.setColumns(10);
+		ad_txt_img.setBounds(108, 0, 130, 35);
+		panel_1.add(ad_txt_img);
+		System.out.println(MyArr.get(0).getImage_file_name());
+		ad_txt_img.setText(MyArr.get(0).getImage_file_name());
+	
 		
 		JPanel panel_8 = new JPanel();
 		panel_8.setLayout(null);
@@ -204,9 +234,11 @@ public class Admin_FilmManagement extends JDialog{
 		panel_9.add(ad_lb_title);
 		
 		ad_txt_title = new JTextField();
+		ad_txt_title.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
 		ad_txt_title.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_9.add(ad_txt_title);
 		ad_txt_title.setColumns(10);
+		ad_txt_title.setText(MyArr.get(0).getTitle());
 		
 		JLabel ad_lb_genre = new JLabel("장르");
 		ad_lb_genre.setHorizontalAlignment(SwingConstants.CENTER);
@@ -214,9 +246,11 @@ public class Admin_FilmManagement extends JDialog{
 		panel_9.add(ad_lb_genre);
 		
 		ad_txt_ganre = new JTextField();
+		ad_txt_ganre.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
 		ad_txt_ganre.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_9.add(ad_txt_ganre);
 		ad_txt_ganre.setColumns(10);
+		ad_txt_ganre.setText(MyArr.get(0).getGenre());
 		
 		JLabel ad_lb_director = new JLabel("감독");
 		ad_lb_director.setHorizontalAlignment(SwingConstants.CENTER);
@@ -224,9 +258,11 @@ public class Admin_FilmManagement extends JDialog{
 		panel_9.add(ad_lb_director);
 		
 		ad_txt_director = new JTextField();
+		ad_txt_director.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
 		ad_txt_director.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_9.add(ad_txt_director);
 		ad_txt_director.setColumns(10);
+		ad_txt_director.setText(MyArr.get(0).getDirector());
 		
 		JLabel ad_lb_age = new JLabel("연령등급");
 		ad_lb_age.setHorizontalAlignment(SwingConstants.CENTER);
@@ -234,9 +270,11 @@ public class Admin_FilmManagement extends JDialog{
 		panel_9.add(ad_lb_age);
 		
 		ad_txt_age = new JTextField();
+		ad_txt_age.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
 		ad_txt_age.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_9.add(ad_txt_age);
 		ad_txt_age.setColumns(10);
+		ad_txt_age.setText(MyArr.get(0).getAge_group());
 		
 		JLabel ad_lb_score = new JLabel("별점");
 		ad_lb_score.setHorizontalAlignment(SwingConstants.CENTER);
@@ -244,9 +282,11 @@ public class Admin_FilmManagement extends JDialog{
 		panel_9.add(ad_lb_score);
 		
 		ad_txt_score = new JTextField();
+		ad_txt_score.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
 		ad_txt_score.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_9.add(ad_txt_score);
 		ad_txt_score.setColumns(10);
+		ad_txt_score.setText(MyArr.get(0).getAverage_score());
 		
 		JLabel ad_lb_gee = new JLabel("등장인물");
 		ad_lb_gee.setHorizontalAlignment(SwingConstants.CENTER);
@@ -254,19 +294,24 @@ public class Admin_FilmManagement extends JDialog{
 		panel_9.add(ad_lb_gee);
 		
 		ad_txt_gee = new JTextField();
+		ad_txt_gee.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
 		ad_txt_gee.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_9.add(ad_txt_gee);
 		ad_txt_gee.setColumns(10);
+		ad_txt_gee.setText(MyArr.get(0).getGee());
 		
 		JLabel ad_lb_openday = new JLabel("개봉일");
 		ad_lb_openday.setHorizontalAlignment(SwingConstants.CENTER);
 		ad_lb_openday.setFont(new Font("Dialog", Font.PLAIN, 15));
 		panel_9.add(ad_lb_openday);
 		
+		String b[] = MyArr.get(0).getOpen_date().split(" ");
 		ad_txt_openday = new JTextField();
+		ad_txt_openday.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
 		ad_txt_openday.setHorizontalAlignment(SwingConstants.CENTER);
 		ad_txt_openday.setColumns(10);
 		panel_9.add(ad_txt_openday);
+		ad_txt_openday.setText(b[0]);
 		
 		JLabel ad_lb_production = new JLabel("제작사");
 		ad_lb_production.setHorizontalAlignment(SwingConstants.CENTER);
@@ -274,9 +319,11 @@ public class Admin_FilmManagement extends JDialog{
 		panel_9.add(ad_lb_production);
 		
 		ad_txt_production = new JTextField();
+		ad_txt_production.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
 		ad_txt_production.setHorizontalAlignment(SwingConstants.CENTER);
 		ad_txt_production.setColumns(10);
 		panel_9.add(ad_txt_production);
+		ad_txt_production.setText(MyArr.get(0).getProduction());
 		
 		JLabel ad_lb_runtime = new JLabel("영화 상영 시간");
 		ad_lb_runtime.setHorizontalAlignment(SwingConstants.CENTER);
@@ -284,9 +331,11 @@ public class Admin_FilmManagement extends JDialog{
 		panel_9.add(ad_lb_runtime);
 		
 		ad_txt_runtime = new JTextField();
+		ad_txt_runtime.setFont(new Font("맑은 고딕 Semilight", Font.PLAIN, 15));
 		ad_txt_runtime.setHorizontalAlignment(SwingConstants.CENTER);
 		ad_txt_runtime.setColumns(10);
 		panel_9.add(ad_txt_runtime);
+		ad_txt_runtime.setText(MyArr.get(0).getRunning_time());
 		
 		JLabel label_20 = new JLabel("영화 정보");
 		label_20.setHorizontalAlignment(SwingConstants.CENTER);
@@ -300,7 +349,11 @@ public class Admin_FilmManagement extends JDialog{
 		label_21.setBounds(232, -10, 232, 40);
 		panel_8.add(label_21);
 		
-		JButton ad_btn_up = new JButton("UP");
+		JButton ad_btn_up = new JButton("등록");
+		ad_btn_up.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		ad_btn_up.setBounds(30, 707, 107, 48);
 		panel.add(ad_btn_up);
 		
@@ -315,7 +368,30 @@ public class Admin_FilmManagement extends JDialog{
 		label_3.setBounds(30, 0, 400, 55);
 		panel.add(label_3);
 		
-		JButton btnDown = new JButton("DOWN");
+		JButton btnDown = new JButton("수정");
+		btnDown.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String title = ad_txt_title.getText();
+				String genre = ad_txt_ganre.getText();
+				String director = ad_txt_director.getText();
+				String age_group = ad_txt_age.getText();
+				String story = textArea.getText();
+				String average_score = ad_txt_score.getText();
+				String gee = ad_txt_gee.getText();
+				String open_date = ad_txt_openday.getText();
+				String production = ad_txt_production.getText();
+				String schedule_date = ad_txt_schedule_date.getText();
+				String image_file_name = ad_txt_img.getText();
+				String running_time = ad_txt_runtime.getText();
+				
+				boolean r = db.changeFilm(film_id, title, genre, director, age_group, story, average_score, gee, open_date, production, schedule_date, image_file_name, running_time);
+				if(r) {
+					JOptionPane.showMessageDialog(null, "영화정보 수정성공");
+				}else
+					JOptionPane.showMessageDialog(null, "영화정보 수정실패");
+			}
+			
+		});
 		btnDown.setBounds(165, 707, 107, 48);
 		panel.add(btnDown);
 	}
