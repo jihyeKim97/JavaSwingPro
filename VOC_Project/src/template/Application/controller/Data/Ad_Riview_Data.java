8c53f625ed92621b7303611c95ed1bc033f79b74
youv132 <youv132@github.com>
2021-06-05 14:23:37 +0900

    <주현> 관리자 리뷰페이지

--- VOC_Project/src/template/Application/controller/Data/Ad_Riview_Data.java
+++ VOC_Project/src/template/Application/controller/Data/Ad_Riview_Data.java
@@ -2,6 +2,77 @@ package template.Application.controller.Data;
 
 public class Ad_Riview_Data {
 
-	
-		
+	String review_id;
+	String content;
+	String star_score;
+	String review_date;
+	String reservation_id;
+	String movies_id;
+
+	public Ad_Riview_Data(String review_id, String content, String star_score, String review_date,
+			String reservation_id, String movies_id) {
+		super();
+		this.review_id = review_id;
+		this.content = content;
+		this.star_score = star_score;
+		this.review_date = review_date;
+		this.reservation_id = reservation_id;
+		this.movies_id = movies_id;
+	}
+
+	public String getReview_id() {
+		return review_id;
+	}
+
+	public void setReview_id(String review_id) {
+		this.review_id = review_id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getStar_score() {
+		return star_score;
+	}
+
+	public void setStar_score(String star_score) {
+		this.star_score = star_score;
+	}
+
+	public String getReview_date() {
+		return review_date;
+	}
+
+	public void setReview_date(String review_date) {
+		this.review_date = review_date;
+	}
+
+	public String getReservation_id() {
+		return reservation_id;
+	}
+
+	public void setReservation_id(String reservation_id) {
+		this.reservation_id = reservation_id;
+	}
+
+	public String getMovies_id() {
+		return movies_id;
+	}
+
+	public void setMovies_id(String movies_id) {
+		this.movies_id = movies_id;
+	}
+
+	@Override
+	public String toString() {
+		return "Ad_Riview_Data [review_id=" + review_id + ", content=" + content + ", star_score=" + star_score
+				+ ", review_date=" + review_date + ", reservation_id=" + reservation_id + ", movies_id=" + movies_id
+				+ "]";
+	}
+
 }
--- VOC_Project/src/template/Application/view/admin/Admin_Riview.java
+++ VOC_Project/src/template/Application/view/admin/Admin_Riview.java
@@ -6,10 +6,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Admin_Riview extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +39,46 @@ public class Admin_Riview extends JFrame {
 	 */
 	public Admin_Riview() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 420, 557);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Review");
+		lblNewLabel.setFont(new Font("맑은 고딕 Semilight", Font.BOLD, 30));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(33, 10, 316, 46);
+		panel.add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 88, 212, 237);
+		panel.add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		
+		JLabel lblComment = new JLabel("Content");
+		lblComment.setHorizontalAlignment(SwingConstants.CENTER);
+		scrollPane.setColumnHeaderView(lblComment);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(236, 88, 146, 46);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("작성아이디");
+		lblNewLabel_1.setBounds(0, 0, 146, 23);
+		panel_1.add(lblNewLabel_1);
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		textField = new JTextField();
+		textField.setBounds(0, 25, 146, 21);
+		panel_1.add(textField);
+		textField.setColumns(10);
 	}
-
 }
